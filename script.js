d97286354839e08d7e74cff48bc4fbf27c618204
Jan Nowaczek <jasiek19_85@o2.pl>
2019-03-26 14:09:22 +0100

    promises_fetch_then_json_

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 const promise = fetch ('https://randomuser.me/api')
-    .then(response => {
-        console.log(response)
-        response.json()
-            .then(data => {
-                console.log(data)
-            } )
-        console.log('inside first then after second then')
-    })
-
-    .then((valueFromPromiseFromThen) => {
-        console.log(valueFromPromiseFromThen)
-    })
+    .then(response =>  response.json())
+    .then(data => console.log(data))
