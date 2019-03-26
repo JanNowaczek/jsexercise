5aeda4fb388cc1dca5d59cc07dafc981070ba372
Jan Nowaczek <jasiek19_85@o2.pl>
2019-03-26 13:50:30 +0100

    promise_inside first then after second then

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const promise = fetch ('https://randomuser.me/api')
-
-
-promise
     .then(response => {
+        console.log(response)
         response.json()
             .then(data => {
                 console.log(data)
             } )
+        console.log('inside first then after second then')
     })
