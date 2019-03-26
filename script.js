70585e0bd01399eaf6ed4637fd06afd7f53978a3
Jan Nowaczek <jasiek19_85@o2.pl>
2019-03-26 12:12:23 +0100

    ex2

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-class Person{
-    constructor(name){
-        this.name = name
+class NumbersContainer {
+    constructor(){
+      this.numbers = []
     }
+  
 
-    changeName(newName){
-        this.name = newName
-
+    addNumber(number){
+        this.numbers = this.numbers.concat(number)
         return this
     }
-}
 
-const person1 = new Person ('Jasiek')
+}
+  
+  const container1 = new NumbersContainer()
+  
+  container1.addNumber(4) 
+  console.log(container1.numbers)
+  
+  container1.addNumber(6) 
+  console.log(container1.numbers)
+  
+  container1
+    .addNumber(6)
+    .addNumber(9)
+    .addNumber(11) 
+
+    console.log(container1.numbers)
